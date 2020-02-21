e380768350bcee30956a53ac62a743aab12a0980
Daniel Podwysocki <daniel@contact24.pl>
2020-02-21 22:24:24 +0100

    update models.py
    added a Seat model representing a seat at the card table and changed relationships to ForeignKey on all models (fixed a mistake)

--- macau_game/models.py
+++ macau_game/models.py
@@ -5,7 +5,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-
 class Game(models.Model):
     '''
         Represents a game and gives us info about it: is it finished and how many players are in it
@@ -13,15 +12,17 @@ class Game(models.Model):
     player_count = models.PositiveSmallIntegerField()
     top_card = models.SmallIntegerField()
     is_finished = models.BooleanField()
-    #number of the starting player, assuming they move in a clockwise fashion and are sorted by id
+    # number of the starting player, assuming they move in a clockwise fashion and are sorted by id
     starting_player = models.SmallIntegerField()
+
+
 class Move(models.Model):
     '''
         A single move: we know which player made the move and what in what game it happened
         We figure out the sequence of moves simply by sorting by ids
     '''
-    player = models.OneToOneField(to=User, on_delete=models.CASCADE)
-    game = models.OneToOneField(to=Game, on_delete=models.CASCADE)
+    player = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    game = models.ForeignKey(to=Game, on_delete=models.CASCADE)
 
 
 class Throw(models.Model):
@@ -30,9 +31,10 @@ class Throw(models.Model):
         We figure out the sequence of throws simply by sorting by ids
 
     '''
-    move = models.OneToOneField(to=Move, on_delete=models.CASCADE)
-    #cards are represented as value (ace=1, king=13) + suit*13(Clubs=0,Hearts=1,Spades=2,Diamonds=3)
+    move = models.ForeignKey(to=Move, on_delete=models.CASCADE)
+    # cards are represented as value (ace=1, king=13) + suit*13(Clubs=0,Hearts=1,Spades=2,Diamonds=3)
     card = models.SmallIntegerField()
+
 
 class Card(models.Model):
     '''
@@ -40,8 +42,17 @@ class Card(models.Model):
         those models get deleted as the cards are thrown
         the reason for not doing this in a session is the possibility of "correspondance" play
     '''
-    game = models.OneToOneField(to=Game, on_delete=models.CASCADE)
-    player = models.OneToOneField(to=User, on_delete=models.CASCADE)
-    #cards are represented as value (ace=1, king=13) + suit*13(Clubs=0,Hearts=1,Spades=2,Diamonds=3)
+    game = models.ForeignKey(to=Game, on_delete=models.CASCADE)
+    player = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    # cards are represented as value (ace=1, king=13) + suit*13(Clubs=0,Hearts=1,Spades=2,Diamonds=3)
     card = models.SmallIntegerField()
-    
+
+
+class Seat(models.Model):
+    '''
+        ties player to game, tells us what seat he's in and if he's finished
+    '''
+    game = models.ForeignKey(to=Game, on_delete=models.CASCADE)
+    player = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    seat_number = models.SmallIntegerField()
+    done = models.BooleanField()
