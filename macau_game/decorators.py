18d6ae5c2ac6ecd09ea2504cc7c7f4ae3db0da7e
Daniel Podwysocki <daniel@contact24.pl>
2020-02-22 16:58:12 +0100

    update: created not_in_game decorator to avoid repetition
    we often need to enforce the player not being in a game (when creating or joining a new one), so we're going to use not_in_game for it

---
+++ macau_game/decorators.py
@@ -0,0 +1,16 @@
+from macau_game.models import Seat
+from django.shortcuts import redirect
+
+
+# check if player is already in game, if he is redirect him to it
+def not_in_game(function):
+    def wrap(request, *args, **kwargs):
+        last_seat = Seat.objects.filter(
+            done=False, player=request.user).count()
+        if last_seat != 0:
+            return(redirect('game'))
+        else:
+            return function(request, *args, **kwargs)
+    wrap.__doc__ = function.__doc__
+    wrap.__name__ = function.__name__
+    return wrap
--- macau_game/views.py
+++ macau_game/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from macau_game import models, forms
 from random import randrange
+from macau_game import decorators
 # Create your views here.
 
 
@@ -15,15 +16,12 @@ def index(request):  # TODO: this view lets you create or join a game
 
 
 @login_required
+@decorators.not_in_game
 def start_game(request):  # TODO: error messages (after error view is finished)
     '''
     this view creates a new game and its settings,
     assigns the player(the one who created it) to it, makes it either public or private, then redirects you to the game view itself
     '''
-    last_seat = models.Seat.objects.filter(
-        done=False, player=request.user).count()
-    if last_seat != 0:
-        return(redirect('game'))
 
     if request.method != 'POST':
         return redirect('error')
@@ -42,15 +40,13 @@ def start_game(request):  # TODO: error messages (after error view is finished)
 
 
 @login_required
+@decorators.not_in_game
 def join_game(request):  # TODO
-
     return HttpResponse('join_game')
 
 
 @login_required
 def game(requst):  # TODO the game view
-    last_move = move.get(player=request.user).order_by(
-        'pk')  # last move made by this player
     return HttpResponse('game')
 
 
