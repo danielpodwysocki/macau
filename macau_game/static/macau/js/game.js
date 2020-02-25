4a4f26dba1951009b8beb65e1506aeb4bff07841
Daniel Podwysocki <daniel@contact24.pl>
2020-02-25 21:16:46 +0100

    update: game.js now grabs data from macau/state json

---
+++ macau_game/static/macau/js/game.js
@@ -0,0 +1,26 @@
+var data;
+
+function update_cards(json) { //TODO: visually represent cards/their amount of each player 
+
+}
+
+
+async function update_json() {
+    fetch("state")
+        .then(resp => resp.json())
+        .then(resp => {
+            if (data != resp) {
+                data = resp;
+                update_cards(data);
+                console.log(JSON.stringify(data));
+            }
+
+        });
+    await new Promise(r => setTimeout(r, 1000)); //apparently async is now hte optimal way to do a "sleep"
+    update();
+}
+
+update();
+
+
+
