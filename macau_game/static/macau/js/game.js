51391e18706a623b6520bdecb3acd209c484ad36
Daniel Podwysocki <daniel@contact24.pl>
2020-02-26 15:35:05 +0100

    update: game view
     now shows player cards (raw values) and amount of opponent cards

--- macau_game/static/macau/js/game.js
+++ macau_game/static/macau/js/game.js
@@ -1,9 +1,35 @@
 var data;
+var players_created = false;
 
-function update_cards(json) { //TODO: visually represent cards/their amount of each player 
+function create_players(state) {
+    let grid = document.getElementById('grid-game_players');
+    for (let i = 0; i < state.player_count - 1; i++) {
+        grid.innerHTML += "<div class='player'>p</div>&#13";
+    }
 
 }
 
+function update_cards(state) { //TODO: visually represent cards/their amount of each player 
+    let players = document.getElementsByClassName("player");
+    console.log(players.length);
+    let user = document.getElementById("game_user_hand");
+    let current_seat = 0; //the seat that we're updating the cards for
+
+    for (let i = 0; i < state.player_count; i++) {
+        if (i == state.position) {
+            user.innerHTML = "";
+
+            for (let j = 0; j < state.hands[i].length; j++) {
+                user.innerHTML += state.hands[i][j] + " "; //TODO: translate it to human values
+            }
+        }
+        else {
+
+            players[current_seat].innerHTML = "";
+            for (let j = 0; j < state.hands[i]; j++) players[current_seat].innerHTML += "c"; //TODO: change to a visual repres. of a card
+        }
+    }
+}
 
 async function update_json() {
     fetch("state")
@@ -11,8 +37,12 @@ async function update_json() {
         .then(resp => {
             if (data != resp) {
                 data = resp;
+                if (!players_created) {
+                    create_players(data);
+                    players_created = true;
+                }
                 update_cards(data);
-                console.log(JSON.stringify(data));
+                console.log(JSON.stringify(data)); //TODO: remove after the view is done
             }
 
         });
